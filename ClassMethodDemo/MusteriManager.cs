ef81157f304353ab4c406729f9ab57afcda1d03c
Göktürk Genç <hgokturkgenc@gmail.com>
2021-01-24 18:26:17 +0300

    Proje dosyası ekle.

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customers = new List<Customer>();
+
+        public void Add(int id, string name, int age)
+        {
+            customers.Add(new Customer()
+            {
+                Id = id,
+                Name = name,
+                Age = age,
+            });
+
+        }
+
+        public void ShowCustomers()
+        {
+            Console.WriteLine("Kayıtlı Müşterilerimiz:");
+            Console.WriteLine("Musteri No \t İsmi \t Yaşi");
+            foreach (Customer customs in customers)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine(customs.Id + " \t " + customs.Name + " \t " + customs.Age);
+            }
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(1, "göktürk", 21);
+            customerManager.Add(2, "mehmet", 28);
+            customerManager.Add(3, "veli", 29);
+            customerManager.Add(4, "mustafa", 37);
+            customerManager.Add(5, "ayhan", 33);
+
+
+            customerManager.ShowCustomers();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
